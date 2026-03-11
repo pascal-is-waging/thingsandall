c8473c25749e8398a61a2c7d2e82cf7554a976aa
ɹ̰ᴉ̰ɯⱯ̰ <95018580+PascalWager@users.noreply.github.com>
2026-03-11 15:14:36 -0400

    sss

--- come.js
+++ come.js
@@ -1,5 +1,17 @@
-const SHEET_ID = "1ThK2AhETarR8HuYoTCNmPFaAL_Kf--cT0BPPSiCQtCo";
+const SHEET_ID = "1aWl4zx1xShGkplD9Glm6iW3LiRHg7m_zBztzmn3NvGA";
 
 fetch(`https://opensheet.elk.sh/${SHEET_ID}/Form Responses 1`)
   .then((res) => res.json())
-  .then((data) => console.log(data));
+  .then((data) => {
+    let g = Object.values(data[0]);
+    console.log("in the fetch");
+    let s = document.querySelector(".fff");
+    const b = document.createElement("div"); //new element with js?
+    b.innerHTML = "ahhh";
+    const div = document.createElement("div");
+    b.innerHTML = `
+      <h2>${g[1]}</h2>
+      <img src="${g[9]}"></img>
+    `;
+    s.appendChild(b);
+  });
