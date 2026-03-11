1c6900109da60719c2e6077f07cfaf097d726342
ɹ̰ᴉ̰ɯⱯ̰ <95018580+PascalWager@users.noreply.github.com>
2026-03-11 14:59:57 -0400

    ss

--- better.js
+++ better.js
@@ -1,5 +1,5 @@
 const SHEET_ID = "1ThK2AhETarR8HuYoTCNmPFaAL_Kf--cT0BPPSiCQtCo";
 
-fetch(`https://opensheet.elk.sh/${SHEET_ID}/Sheet1`)
+fetch(`https://opensheet.elk.sh/${SHEET_ID}/Form Responses 1`)
   .then((res) => res.json())
   .then((data) => console.log(data));
