73393bb6852d44c5b49a57596c46455444e6e04c
ɹ̰ᴉ̰ɯⱯ̰ <95018580+PascalWager@users.noreply.github.com>
2026-04-08 12:09:29 -0400

    sss

--- uh.js
+++ uh.js
@@ -1,27 +1,42 @@
 const SHEET_ID = "1aWl4zx1xShGkplD9Glm6iW3LiRHg7m_zBztzmn3NvGA";
 
-fetch(`https://opensheet.elk.sh/${SHEET_ID}/Form Responses 1`)
+// fetch(`https://opensheet.elk.sh/${SHEET_ID}/ALL`)
+//   .then((res) => res.json())
+//   .then((data) => {
+//     let g = Object.values(data[0]);
+//     console.log(g);
+//     console.log("in the fetch");
+//     let s = document.querySelector(".fff");
+//     const b = document.createElement("div"); //new element with js?
+//     b.innerHTML = "ahhh";
+//     const div = document.createElement("div");
+//     // const url = getDriveImageUrl(`${g[9]}`);
+//     b.innerHTML = `
+//       <h2>${g[1]}</h2>
+//       <img src="${url}"></img>
+//     `;
+//     s.appendChild(b);
+//   });
+
+// function getDriveImageUrl(driveLink) {
+//   const match = driveLink.match(/[?&]id=([^&]+)/);
+//   if (!match) return null;
+
+//   const fileId = match[1];
+//   return `https://drive.google.com/uc?export=view&id=${fileId}`;
+// }
+
+fetch(
+  `https://opensheet.elk.sh/1Km1had78v2jOwEJU0qGKknrBSOaY-z-L-LJ-dC7pbBM/ALL`,
+)
   .then((res) => res.json())
   .then((data) => {
-    let g = Object.values(data[0]);
-    console.log(g);
-    console.log("in the fetch");
-    let s = document.querySelector(".fff");
-    const b = document.createElement("div"); //new element with js?
-    b.innerHTML = "ahhh";
-    const div = document.createElement("div");
-    const url = getDriveImageUrl(`${g[9]}`);
-    b.innerHTML = `
-      <h2>${g[1]}</h2>
-      <img src="${url}"></img>
-    `;
-    s.appendChild(b);
+    console.log(data);
   });
 
-function getDriveImageUrl(driveLink) {
-  const match = driveLink.match(/[?&]id=([^&]+)/);
-  if (!match) return null;
+const params = new URLSearchParams(window.location.search);
+let slug = params.get("slug");
 
-  const fileId = match[1];
-  return `https://drive.google.com/uc?export=view&id=${fileId}`;
-}
+console.log(slug);
+
+window.history.pushState({}, "", `/${slug}`);
